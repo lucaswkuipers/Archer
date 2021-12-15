ebdec354957b7150ce283b96658aef2f82ea62ec
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2021-12-15 02:32:11 -0300

    Add base movement

--- Archer/GameScene.swift
+++ Archer/GameScene.swift
@@ -2,15 +2,17 @@ import SpriteKit
 import GameController
 
 final class GameScene: SKScene {
+    let playerSpeedFactor: Float = 2
+    let playerMaxSpeed: Float = 100
     var controller: GCController?
     let controllerManager = ControllerManager()
 
     let ground: SKNode = {
         let node = SKShapeNode(rectOf: CGSize(width: 1000, height: 100))
         node.name = "ground"
+        node.fillColor = .white
         node.physicsBody = SKPhysicsBody(rectangleOf: node.frame.size)
         node.physicsBody?.isDynamic = false
-        node.fillColor = .white
         return node
     }()
 
@@ -46,6 +48,12 @@ final class GameScene: SKScene {
         node.name = "player"
         node.fillColor = .red
         node.physicsBody = SKPhysicsBody(rectangleOf: node.frame.size)
+        node.physicsBody?.linearDamping = 0.5
+        node.physicsBody?.angularDamping = 0.5
+        let rotationDegrees = CGFloat.pi / 2
+        let rotationRange = SKRange(lowerLimit: -rotationDegrees, upperLimit: rotationDegrees)
+        let rotationConstraint = SKConstraint.zRotation(rotationRange)
+        node.constraints = [rotationConstraint]
         return node
     }()
 
@@ -61,6 +69,22 @@ final class GameScene: SKScene {
         ceiling.position = CGPoint(x: 0, y: frame.maxY)
         player.position = CGPoint(x: 0, y: frame.midY)
         controllerManager.delegate = self
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+        guard let gamepad = controller?.extendedGamepad else { return }
+        let horizontalMovementInput = gamepad.leftThumbstick.xAxis.value
+        let verticalMovementInput = gamepad.leftThumbstick.yAxis.value
+        let horizontalAimInput = gamepad.rightThumbstick.xAxis.value
+        let verticalAimInput = gamepad.rightThumbstick.yAxis.value
+        move(dx: horizontalMovementInput, dy: verticalMovementInput)
+    }
+
+    private func move(dx: Float, dy: Float) {
+        let dx = CGFloat(dx * playerSpeedFactor)
+        let dy = CGFloat(dy * playerSpeedFactor)
+
+        player.physicsBody?.applyImpulse(CGVector(dx: dx, dy: dy))
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
